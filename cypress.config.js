48dcc2908b57d12518148744a7bf2d9b8e8b6dd1
itsash111287 <itsash111287@gmail.com>
2024-05-12 23:34:53 +0530

    latest

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
+    supportFile: false,
     setupNodeEvents(on, config) {
       
       // implement node event listeners here
